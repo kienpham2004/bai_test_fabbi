08e1b05f8d0488fcc5645560a9c0baabd002fe2f
kienpham2004 <phamkien20041998@gmail.com>
2021-03-17 16:48:56 +0700

    v2

--- bai_test_pham_van_kien/bai1.php
+++ bai_test_pham_van_kien/bai1.php
@@ -1,17 +1,9 @@
 <?php
-$arr = [11, 2, 8, 10, 5, 99, 1, 8, 9];
+$array = [11, 2, 8, 10, 5, 99, 1, 8, 9];
 
-function check($arr, $n){
-    while($n % 3 == 0){
-        for($i = 0; $i < count($arr); $i++){
-            for($j = $i + $n; $j < $i; $j+=$n){
-                if($i < $j){
-                    $tmp = $arr[$i];
-                    $arr[$i] = $arr[$j];
-                    $arr[$j] = $tmp;
-                }
-            }
-        }
-    }
+$length = count($array)/3;
+$newarray = [];
+$cont =0;
+for($i = 0; $i < $length; $i++) {
+   echo $array[$i] . "-" . $array[$length + $i]   . '-' . $array[2*$length + $i] . '-';
 }
-
--- bai_test_pham_van_kien/bai3.php
+++ bai_test_pham_van_kien/bai3.php
@@ -1,24 +1,15 @@
 <?php
-$arr = [1, 5, 4, 7, 9, 0, -10, 13, 93, 14, 15];
-$khoangcach = 9;
-$array = [];
-function kiemtra($arr, $khoangcach){
-    for($i = 0; $i<count($arr) - 1; $i++ ){
-        for ($j= $i + 1; $j < count($arr); $j++) { 
-            $a = $arr[$i] - $arr[$j];
-            if($a >0 && $a < $khoangcach){
-                $khoangcach = $arr[$j] - $arr[$j];
-            }
-        }
-    }
+$array = [1, 5, 4, 7, 9, 0, -10, 13, 93, 14, 15];
 
-    for ($i=0; $i < count($arr) - 1; $i++) { 
-        for ($j=$i + 1; $j < count($arr); $j++) { 
-            $a = $arr[$i] - $a[$j];
-            if($a === $khoangcach || $a === -$khoangcach){
-                $c = [$arr[$i], $arr[$j]];
-                array_push($array,$c);
-            }
-        }
+rsort($array);
+$sub = abs($array[0] - $array[1]);
+for($i = 0; $i < count($array) - 1; $i++) {
+    if ($sub > abs($array[$i] - $array[$i+1])) {
+        $sub = abs($array[$i] - $array[$i+1]);
+    }
+}
+for ($i = 0; $i < count($array) - 1; $i++) {
+    if ($sub == abs($array[$i] - $array[$i+1])) {
+        echo $array[$i] . '-' . $array[$i+1] . "   ";
     }
 }
